11fc2ba282fa36855858869c45c4ec4ea0079bf7
Enshen Zhu <winniecmz.zhu@gmail.com>
2022-07-04 19:05:07 -0400

    finish the learning of the data type and basci string operations

---
+++ Data_Types/Data_Types/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data_Types
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string phrase = "Silencer";
+            char grade = 'A'; //    you can only put one single character inside the char_typed variable
+            int age = 30;
+
+            //  there are three different ways to store decimal numbers in C#
+            //  float - least precise
+            //  double
+            //  decimal - most precise (like money)
+            float height;
+            double gpa = 3.2;
+            decimal wage;
+
+            bool isMale = false; //  True or false
+
+            Console.WriteLine(phrase);
+            Console.WriteLine(grade);
+            Console.WriteLine(age);
+            Console.WriteLine(gpa);
+            Console.WriteLine(isMale);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Working_with_strings/Working_with_strings/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Working_with_strings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string phrase1 = "Silencer\nHerald" + "\nis cool";
+            string phrase2 = "\n";
+            Console.WriteLine(phrase1.Length);
+            Console.WriteLine(phrase1[0]); // print out the first character of the string
+            Console.WriteLine(phrase1.IndexOf("le"));
+            // print out the exact location of where the character or substring is located inside the string;
+            // return -1 if the target cannot be found
+
+            Console.WriteLine(phrase1.Substring(2, 4));
+            // grab the substring from the original string
+            // the first argument is the starting point
+            // the second argument shows how many characters we are going to grab
+
+            Console.WriteLine(phrase1.ToUpper());
+            Console.WriteLine(phrase1.Contains("cool")); // judge whether the string contains a sub-string
+
+            Console.WriteLine(phrase2.Length);
+            Console.ReadLine();
+        }
+    }
+}
