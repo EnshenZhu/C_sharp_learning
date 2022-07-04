f815f6257c8e43a14b4dab575a59c7e424450159
Enshen Zhu <winniecmz.zhu@gmail.com>
2022-07-04 11:49:13 -0400

    finish the learning of c# basic variable opts

--- Silencer/Silencer/Program.cs
+++ Silencer/Silencer/Program.cs
@@ -11,7 +11,7 @@ namespace Silencer
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World");
-            Console.ReadLine();
+            Console.ReadLine(); 
         }
     }
 }
---
+++ Variables/Variables/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Variables
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string characterName = "John";
+
+            int characterAge;
+            characterAge = 35;
+
+            Console.WriteLine("There once was a man named " + characterName);
+            Console.WriteLine("He was " + characterAge + " years old");
+
+            characterName = "Mike";
+            Console.WriteLine("He really liked the name " + characterName);
+            Console.WriteLine("But didn't like being " + characterAge);
+
+            Console.ReadLine();
+        }
+    }
+}
