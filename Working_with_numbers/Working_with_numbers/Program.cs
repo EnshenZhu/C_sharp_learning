4467e48af5c046b5213888c7e9bcd2ff7f3b3e2a
Enshen Zhu <winniecmz.zhu@gmail.com>
2022-07-04 22:32:31 -0400

    finish learning the working with numbers

---
+++ Working_with_numbers/Working_with_numbers/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Working_with_numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(5 + 8); // this will output 13
+            Console.WriteLine(5 + 8.2); // this will output 13.2
+
+            Console.WriteLine(5 % 2);
+
+            Console.WriteLine(5 / 2);  // this will output 2
+            Console.WriteLine(5 / 2.0); // this will output 2.5
+            
+            int num = 6;
+            Console.WriteLine(--num);
+
+            Console.WriteLine(-num);
+
+            Console.WriteLine(Math.Abs(num));
+
+            Console.WriteLine(Math.Pow(num, 0.5));
+            Console.WriteLine(Math.Sqrt(num));
+
+            Console.ReadLine();
+        }
+    }
+}
