42ece0ff1aaf8ce0b466575034042f3283e9b1e5
Enshen Zhu <winniecmz.zhu@gmail.com>
2022-07-05 01:07:48 -0400

    finish the basic of void Methods

---
+++ Methods/Methods/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods
+{
+    internal class Program
+    {
+        static void Main(string[] args) // Main method
+        {
+            SayHi("Peter",33);
+            SayHi("Tome",25);
+            SayHi("John",14);
+            Console.ReadLine();
+        }
+
+        static void SayHi(string Name,int age) // this method does not return anything, which does void return
+        {
+            Console.WriteLine("Hello, " + Name+" your are "+age);
+        }
+    }
+}
