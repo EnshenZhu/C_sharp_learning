7c25dd7fcc0dbfde9e63140ad74befa7e3a880b6
Enshen Zhu <winniecmz.zhu@gmail.com>
2022-07-05 00:55:49 -0400

    finish the basic C# array learning

---
+++ Array/Array/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Array
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] luckyNumbers1 = { 4, 8, 15, 16, 23, 42 }; // create an array of integers
+            
+            Console.WriteLine(luckyNumbers1[0]);
+
+            int[] luckyNumbers2 = luckyNumbers1; // soft copy
+
+            int[] luckyNumbers3 = new int[luckyNumbers1.Length]; // hard copy
+            for (int i = 0; i < luckyNumbers3.Length; i++)
+            {
+                luckyNumbers3[i] = luckyNumbers1[i];
+            }
+
+            luckyNumbers1[0] = 12;
+            Console.WriteLine(luckyNumbers2[0]);
+            Console.WriteLine(luckyNumbers3[0]);
+
+            string[] friends = new string[10];
+            friends[0] = "Tommy";
+            friends[1] = "Bob";
+
+            Console.WriteLine(friends);
+
+            Console.ReadLine();
+        }
+    }
+}
