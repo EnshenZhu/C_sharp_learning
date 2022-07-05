d1483c4d6d2a4659e35158f455bdc15aee0528fa
Enshen Zhu <winniecmz.zhu@gmail.com>
2022-07-05 01:59:47 -0400

    finish the switch statement and the while loop statement, skip the if statement

---
+++ Switch_statement/Switch_statement/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Switch_statement
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(GetDay(4));
+            Console.WriteLine(GetDay(0));
+            Console.WriteLine(GetDay(12));
+            Console.WriteLine(GetDay(3));
+
+            Console.ReadLine();
+        }
+
+        static string GetDay(int dayNum)
+        {
+            string dayName;
+
+            switch (dayNum)
+            {
+                case 0:
+                    dayName = "Sunday";
+                    break;
+                case 1:
+                    dayName = "Monday";
+                    break;
+                case 2:
+                    dayName = "Tuesday";
+                    break;
+                case 3:
+                    dayName = "Wednesday";
+                    break;
+                case 4:
+                    dayName = "Thursday";
+                    break;
+                case 5:
+                    dayName = "Friday";
+                    break;
+                case 6:
+                    dayName = "Saturday";
+                    break;
+                default:
+                    dayName = "Invalid Day Number";
+                    break;
+            }
+
+            return dayName;
+        }
+    }
+}
---
+++ While_loop/While_loop/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace While_loop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("part1");
+            int index = 1;
+            while (index<=5)
+            {
+                Console.WriteLine(index);
+                index++;
+            }
+
+            Console.WriteLine("part2");
+            int index2 = 6;
+            do
+            {
+                Console.WriteLine(index);
+                index++;
+            }while (index2<5); // the do...while loop will execute the loop before checking the condition
+            
+            Console.ReadLine();
+        }
+    }
+}
