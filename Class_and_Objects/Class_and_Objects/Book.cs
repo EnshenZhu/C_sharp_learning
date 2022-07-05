0b3e6fef01fbf49a7dda26fa9b266318a506bb76
Enshen Zhu <winniecmz.zhu@gmail.com>
2022-07-05 10:21:37 -0400

    upgrade the ordinary inputs for ojects with simple constructors

--- Class_and_Objects/Class_and_Objects/Book.cs
+++ Class_and_Objects/Class_and_Objects/Book.cs
@@ -12,5 +12,18 @@ namespace Class_and_Objects
         public string author;
         public int pages;
         // the above three are public class attributes
+
+        public Book(string aTitle, string aAuthor, int aPages) // constructor
+        {
+            title = aTitle;
+            author = aAuthor;
+            pages = aPages;
+        }
+
+        public Book() // you can have multiple constructors
+        {
+
+        }
+        
     }
 }
--- Class_and_Objects/Class_and_Objects/Program.cs
+++ Class_and_Objects/Class_and_Objects/Program.cs
@@ -10,21 +10,26 @@ namespace Class_and_Objects
     {
         static void Main(string[] args)
         {
-            Book book1 = new Book(); // this is an instance of a book class
-            book1.title = "Harry Potter";
-            book1.author = "JK Rowling";
-            book1.pages = 400;
+            Book book1 = new Book("Harry Potter", "JK Rowling",400); // this is an instance of a book class
+            //book1.title = "Harry Potter";
+            //book1.author = "JK Rowling";
+            //book1.pages = 400;
 
             Console.WriteLine(book1.title);
             Console.WriteLine(book1.pages);
 
-            Book book2 = new Book(); // this is an instance of a book class
-            book2.title = "Lord Of the Rings";
-            book2.author = "Tolkein";
-            book2.pages = 700;
+            Book book2 = new Book("Lord Of the Rings", "Tolkein", 700); // this is an instance of a book class
+            //book2.title = "Lord Of the Rings";
+            //book2.author = "Tolkein";
+            //book2.pages = 700;
 
             Console.WriteLine(book2.author);
             Console.WriteLine(book2.pages);
+
+            Book book3 = new Book();
+            book3.title = "Hobby";
+
+            Console.WriteLine(book3.title);
 
             Console.ReadLine();
         }
