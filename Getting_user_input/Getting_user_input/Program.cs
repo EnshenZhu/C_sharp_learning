3968810df0c96a71e6540229d6c9275abcdae9d4
Enshen Zhu <winniecmz.zhu@gmail.com>
2022-07-04 22:44:49 -0400

    finish the basic prompt of user input

---
+++ Getting_user_input/Getting_user_input/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Getting_user_input
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter your name: "); // print out on the same line
+            string name=Console.ReadLine(); // prompt the user to enter
+
+            Console.Write("Enter your age: "); // print out on the same line
+            string age = Console.ReadLine(); // prompt the user to enter
+
+            Console.WriteLine("Hello "+name+", your are "+age);
+
+            Console.ReadLine();
+        }
+    }
+}
