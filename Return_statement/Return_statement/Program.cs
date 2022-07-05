c43075a85fc3b9b1cd59cce5d3b3a821c8965cfd
Enshen Zhu <winniecmz.zhu@gmail.com>
2022-07-05 01:14:17 -0400

    finish the basic learning of a return method

---
+++ Return_statement/Return_statement/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Return_statement
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double cubedNumer = Cube(5);
+            
+            Console.WriteLine(cubedNumer);
+            Console.ReadLine();
+        }
+
+        static double Cube(double num)
+        {
+            double result = num * num * num;
+            return result;
+        }
+    }
+}
